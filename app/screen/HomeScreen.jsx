c5b4ceb2a01138d3857dac6612d3b27bc3189586
JyToSTT <jean.tostivint@gmail.com>
2021-04-19 15:24:58 +0200

    > Adding home style

--- app/screen/HomeScreen.jsx
+++ app/screen/HomeScreen.jsx
@@ -1,37 +1,97 @@
 import React, {useEffect, useState} from 'react';
-import {Alert, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+
 import UserApi from "../src/api/UserApi";
+import {ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: StatusBar.currentHeight,
+//   },
+//   scrollView: {
+//     backgroundColor: 'pink',
+//     marginHorizontal: 20,
+//   },
+//   text: {
+//     fontSize: 42,
+//   },
+// });
+
 const HomeScreen = ({navigation}) => {
 
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        const fetchDataAsync = async () => {
-            setUsers(await UserApi.getUsers());
-        }
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      setUsers(await UserApi.getUsers());
+    }
 
-        fetchDataAsync()
-    }, [])
+    fetchDataAsync()
+  }, [])
 
-    return (
-        <View style={styles.container}>
+  return (
+    <View style={[{
+      flex: 1,
+      padding: 20,
+    }, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+      <View style={{flex: 1, backgroundColor: "purple"}}/>
+
+      <View style={{flex: 6, backgroundColor: "red"}}>
+        <ScrollView style={{flex: 6, backgroundColor: 'red'}}>
+          <View style={styles.container}>
             {
-                users.map(user => {
-                    return <TouchableOpacity onPress={() => {
-                        navigation.navigate('UserDetails', {id: user.id})
-                    }}>
-                        <Text>{user.name}</Text>
-                    </TouchableOpacity>
-                })
+              users.map(user => {
+                return <TouchableOpacity onPress={() => {
+                  navigation.navigate('UserDetails', {id: user.id})
+                }}>
+                  <Text style={{height: 80}}>{user.name}</Text>
+                </TouchableOpacity>
+              })
             }
-        </View>
-    )
+          </View>
+        </ScrollView>
+      </View>
+      <View style={{flex: 2, backgroundColor: "darkorange"}}/>
+      <View style={{flex: 3, backgroundColor: "green"}}/>
+    </View>
+
+
+
+    // <SafeAreaView style={styles.container}>
+    //     <ScrollView style={{flex: 1, backgroundColor: 'red'}}>
+    //         <Text style={styles.text}>
+    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+    //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    //             aliquip ex ea commodo consequat. Duis aute irure dolor in
+    //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    //             culpa qui officia deserunt mollit anim id est laborum.
+    //         </Text>
+    //     </ScrollView>
+    //     <ScrollView style={{flex: 2, backgroundColor: 'pink'}}>
+    //         <Text style={styles.text}>
+    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+    //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    //             aliquip ex ea commodo consequat. Duis aute irure dolor in
+    //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    //             culpa qui officia deserunt mollit anim id est laborum.
+    //         </Text>
+    //     </ScrollView>
+    // </SafeAreaView>
+  )
 }
 
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 6,
         backgroundColor: 'blue'
     },
 });

fbe7054162e4c914b6c80e6de86f9622e3e4d74c
RomainDreidemy <dreidemyromain@gmail.com>
2021-04-20 09:46:30 +0200

    Avancement userDetails - .map() => Flatlist

--- app/component/UserDetails/TodoItem.jsx
+++ app/component/UserDetails/TodoItem.jsx
@@ -15,9 +15,10 @@ const TodoItem = ({id, title, completed}) => {
 
 
     return (
-        <TouchableOpacity style={tailwind(  `h-12 bg-${isCompleted ? 'green' : 'red'}-300 mb-2 flex-1 flex-row justify-between p-2 items-center border rounded-lg border-transparent`)} onPress={() => {
-            navigation.navigate('TodoDetails', {id})
-        }}>
+        <TouchableOpacity
+            style={tailwind(  `h-12 bg-${isCompleted ? 'green' : 'red'}-200 mb-2 flex-row justify-between p-2 items-center border rounded-lg border-transparent`)}
+                          onPress={() => navigation.navigate('TodoDetails', {id})}
+        >
             <Text style={tailwind('text-white')}>{title.substring(0, 30)}{title.length > 30 ? '...' : ''}</Text>
             <Switch
                 trackColor={{ false: "red", true: "green" }}
--- app/screen/UserDetailsScreen.jsx
+++ app/screen/UserDetailsScreen.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView, Text, View} from "react-native";
+import {FlatList, Text, View} from "react-native";
 import UserApi from "../src/api/UserApi";
 import tailwind from "tailwind-rn";
 import TodoItem from "../component/UserDetails/TodoItem";
@@ -20,7 +20,7 @@ const UserDetailsScreen = ({ route }) => {
     }, []);
 
     return (
-        <ScrollView style={tailwind('p-5')}>
+        <View style={tailwind('p-2 flex-1')}>
             <View style={tailwind('border rounded-lg p-2 bg-white border-gray-200')}>
                 <Text style={tailwind('mb-1')}>Nom: {user.name}</Text>
                 <Text style={tailwind('mb-1')}>Société: {user.company?.name}</Text>
@@ -28,18 +28,25 @@ const UserDetailsScreen = ({ route }) => {
                 <Text style={tailwind('mb-1')}>Téléphone: {user.phone}</Text>
             </View>
 
-            <Text style={tailwind('text-xl mt-3 mb-3')}>TODOs</Text>
-            {
-                todos.map(todo => <TodoItem key={todo.id} id={todo.id} title={todo.title} completed={todo.completed}/>)
-            }
-
-            <Text style={tailwind('text-xl mt-3 mb-3')}>Albums</Text>
-            <View style={tailwind('h-8 bg-gray-500')}>
+            <View style={{flex: 3}}>
+                <Text style={tailwind('text-xl mt-3 mb-3')}>TODOs</Text>
+                <FlatList
+                    data={todos}
+                    keyExtractor={({id}) => id}
+                    renderItem={({item}) => <TodoItem key={item.id} id={item.id} title={item.title} completed={item.completed}/>}
+                />
             </View>
-            {
-                albums.map(album => <AlbumItem id={album.id} title={album.title}/>)
-            }
-        </ScrollView>
+
+            <View style={{flex: 2}}>
+                <Text style={tailwind('text-xl mt-3 mb-3')}>Albums</Text>
+                <View style={tailwind('h-8 bg-gray-500')} />
+                <FlatList
+                    data={albums}
+                    keyExtractor={({id}) => id}
+                    renderItem={({item}) => <AlbumItem id={item.id} title={item.title}/>}
+                />
+            </View>
+        </View>
     )
 }
 

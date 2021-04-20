bf8eab2a4a76cb4ef414f94f9e359679d142bb8e
JyToSTT <jean.tostivint@gmail.com>
2021-04-20 12:16:29 +0200

    > Cleanup package

--- app/screen/HomeScreen.jsx
+++ app/screen/HomeScreen.jsx
@@ -5,21 +5,6 @@ import {ScrollView, StyleSheet, View,Text} from 'react-native';
 import MapView, {Marker} from "react-native-maps";
 import HomeUser from "../component/HomeUser";
 
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     paddingTop: StatusBar.currentHeight,
-//   },
-//   scrollView: {
-//     backgroundColor: 'pink',
-//     marginHorizontal: 20,
-//   },
-//   text: {
-//     fontSize: 42,
-//   },
-// });
-
 const HomeScreen = ({navigation}) => {
 
   const [users, setUsers] = useState([]);

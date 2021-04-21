3c850d6624877755a0a858f9fdcf1f235110b1a6
RomainDreidemy <dreidemyromain@gmail.com>
2021-04-21 10:09:28 +0200

    Add style in PostDetailsScreen.jsx + Add comment

--- app/component/PostComment.jsx
+++ app/component/PostComment.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-const PostComment = ({ username, comment }) => (
-  <View>
-    <Text>{username}</Text>
-    <Text>{comment}</Text>
+const PostComment = ({ comment, title }) => (
+  <View style={styles.container}>
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.content}>{comment.replace(/\n|\r/g,'')}</Text>
   </View>
 )
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 30,
+        flex: 1
+    },
+    title: {
+        fontSize: 16,
+        marginBottom: 5
+    },
+    content: {
+        fontStyle: 'italic'
+    }
+})
 
 export default PostComment;
--- app/component/PostContent.jsx
+++ app/component/PostContent.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import {View, Text, StyleSheet, TextInput} from "react-native";
 
 const PostContent = ({ title, body }) => (
-  <View>
-    <Text>{title}</Text>
-    <Text>{body}</Text>
+  <View style={styles.container}>
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.body}>{body}</Text>
   </View>
 )
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 22,
+        marginBottom: 15
+    },
+    body: {
+        marginBottom: 40
+    }
+})
 
 export default PostContent;

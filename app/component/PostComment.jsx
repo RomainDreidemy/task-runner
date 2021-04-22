c1698b9619fb76dc9d81f0aaca84baf5ac6efbb5
RomainDreidemy <dreidemyromain@gmail.com>
2021-04-22 15:12:34 +0200

    push pour le prof

--- app/component/PostComment.jsx
+++ app/component/PostComment.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 const PostComment = ({ comment, title }) => (
   <View style={styles.container}>
-    <Text style={styles.title}>{title}</Text>
+      <View style={{flexDirection: 'row', alignItems: 'center', marginBottom: 5}}>
+          <View style={styles.userImage}>
+              <Ionicons style={styles.userIcon} name="person" size={15} color="white" />
+          </View>
+          <View>
+              <Text style={styles.userText}>John Doe </Text>
+          </View>
+      </View>
     <Text style={styles.content}>{comment.replace(/\n|\r/g,'')}</Text>
   </View>
 )
@@ -15,11 +23,26 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 16,
-        marginBottom: 5
+        // marginBottom: 5
     },
     content: {
-        fontStyle: 'italic'
-    }
+        fontStyle: 'italic',
+        marginLeft: 30
+    },
+    userText: {
+        fontSize: 15,
+
+    },
+    userImage: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 25,
+        width: 25,
+        backgroundColor: '#e6e4e6',
+        borderRadius: 500,
+        marginRight: 7
+    },
 })
 
 export default PostComment;

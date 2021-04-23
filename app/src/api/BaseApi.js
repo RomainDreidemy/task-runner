8d28a96e12e4cbb5d88ed0bd006dee0c41f55254
Guillaume <guillaume.trb@hotmail.com>
2021-04-23 10:46:24 +0200

    fix: changes api url

--- app/src/api/BaseApi.js
+++ app/src/api/BaseApi.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 
 class BaseApi {
-    API_URL=`https://jsonplaceholder.typicode.com`
+  API_URL = `https://jsonplaceholder.cypress.io`;
 
-    async get(url) {
-        return await axios.get(`${this.API_URL}${url}`);
-    }
+  async get(url) {
+    return await axios.get(`${this.API_URL}${url}`);
+  }
 
-    // async post(url, body: object = {}) {
-    //     return await axios.post(`${this.API_URL}${url}`,body ,{
-    //         headers: {
-    //             'Authorization': await _retrieveData('user_token') ?? ''
-    //         }});
-    // }
-    //
-    // async delete(url: string) {
-    //     return await axios.delete(`${this.API_URL}${url}`,{
-    //         headers: {
-    //             'Authorization': await _retrieveData('user_token') ?? ''
-    //         }});
-    // }
-    //
-    // async patch(url: string, body: object = {}) {
-    //     return await axios.patch(`${this.API_URL}${url}`,body ,{
-    //         headers: {
-    //             'Authorization': await _retrieveData('user_token') ?? ''
-    //         }});
-    // }
+  // async post(url, body: object = {}) {
+  //     return await axios.post(`${this.API_URL}${url}`,body ,{
+  //         headers: {
+  //             'Authorization': await _retrieveData('user_token') ?? ''
+  //         }});
+  // }
+  //
+  // async delete(url: string) {
+  //     return await axios.delete(`${this.API_URL}${url}`,{
+  //         headers: {
+  //             'Authorization': await _retrieveData('user_token') ?? ''
+  //         }});
+  // }
+  //
+  // async patch(url: string, body: object = {}) {
+  //     return await axios.patch(`${this.API_URL}${url}`,body ,{
+  //         headers: {
+  //             'Authorization': await _retrieveData('user_token') ?? ''
+  //         }});
+  // }
 }
 
 export default BaseApi;
